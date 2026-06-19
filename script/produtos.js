db4b3c7002d86bd6b6a921dfd6c98a2482575306
cogumelies <cogumelosifesdocs@gmail.com>
2026-06-19 18:36:55 -0300

    Descrição dos produtos no vetor

--- script/produtos.js
+++ script/produtos.js
@@ -3,33 +3,33 @@ var vetProdutos = [
         nome: "Caixa Artesanal Bombons Napolitano, 175g",
         preco: 34.99,
         categoria: "Bombons",
-        descricao: ""
+        descricao: "Caixa artesanal de bombons napolitano, com uma deliciosa combinação de chocolate ao leite, chocolate e chocolate branco sabor morango."
     },
     {
         nome: "Caixa Coração Bombons ao Leite, 185g",
         preco: 44.99,
         categoria: "Bombons",
-        descricao: ""
+        descricao: "Caixa em formato de coração com bombons ao leite, chocolate simples porém com uma cremosidade única, ideal para presentear alguém especial."
     },
     {
         nome: "Caixa Bombons Zero Açúcar, 135g",
         preco: 24.99,
         categoria: "Zero Açúcar",
-        descricao: ""
+        descricao: "Caixa de bombons zero açúcar, feita com chocolate ao leite sem adição de açúcares, perfeita para quem busca uma opção mais saudável sem abrir mão do sabor."
     },
 
     {
         nome: "Barra Chocolate Recheada Avelã 100g",
         preco: 19.99,
         categoria: "Barras",
-        descricao: ""
+        descricao: "Uma barra de chocolate recheada com avelã, combinando o sabor intenso do chocolate com a crocância e o sabor marcante da avelã."
     }
     ,
     {
         nome: "Barra Chocolate Recheada Morango 100g",
         preco: 19.99,
         categoria: "Barras",
-        descricao: ""
+        descricao: "Uma barra de chocolate recheada com morango, oferecendo uma combinação perfeita entre o sabor doce do chocolate e a acidez do morango."
     }
     
 ];
