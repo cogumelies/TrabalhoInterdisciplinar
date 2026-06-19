db5d7829b07a6f439786a7702bee1624dcaa191a
JuioFD <dossifuteboL@gmail.com>
2026-06-19 17:08:47 -0300

    vetor inicial de produtos adicionado

---
+++ script/produtos.js
@@ -0,0 +1,35 @@
+const produtos = [
+    {
+        nome: "Caixa Artesanal Bombons Napolitano, 175g",
+        preco: 34.99,
+        categoria: "Bombons",
+        descricao: ""
+    },
+    {
+        nome: "Caixa Coração Bombons ao Leite, 185g",
+        preco: 44.99,
+        categoria: "Bombons",
+        descricao: ""
+    },
+    {
+        nome: "Caixa Bombons Zero Açúcar, 135g",
+        preco: 24.99,
+        categoria: "Zero Açúcar",
+        descricao: ""
+    },
+
+    {
+        nome: "Barra Chocolate Recheada Avelã 100g",
+        preco: 19.99,
+        categoria: "Barras",
+        descricao: ""
+    }
+    ,
+    {
+        nome: "Barra Chocolate Recheada Morango 100g",
+        preco: 19.99,
+        categoria: "Barras",
+        descricao: ""
+    }
+    
+];
